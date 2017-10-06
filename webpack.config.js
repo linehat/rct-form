5c8394b7c65dd741dd3d3be74ee3fa187560db5b
GitNiko <galaxis.ling@gmail.com>
2017-10-06 16:23:09 +0800

    project config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // 为 webpack-dev-server 的环境打包代码
     // 然后连接到指定服务器域名与端口
 
-    "webpack/hot/only-dev-server",
+    // "webpack/hot/only-dev-server",
     // 为热替换(HMR)打包好代码
     // only- 意味着只有成功更新运行代码才会执行热替换(HMR)
 
